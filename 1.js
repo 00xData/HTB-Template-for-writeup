38b4f28dd7e47a2e54c537c038bd4ea31ca73196
xData <28519465+00xData@users.noreply.github.com>
2021-07-24 15:24:09 +0000

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,7 @@
+<html><head><title>InternalPage</title></head><body>
+<table><th>something…
+<td><script>/*</td><td>something…</td>
+<td>*/My JS attack payload/*</td><td>something…</td>
+<td>*/My JS attack payload/*</td><td>something…</td>
+<td>*/</script></td><td>something…</td>
+</table></body></html>
