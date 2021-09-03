7ad33834a1d714acf89378c3b47151586da87653
xData <28519465+00xData@users.noreply.github.com>
2021-09-02 20:04:41 -0500

    Delete 1.js

--- 1.js
+++
@@ -1,7 +0,0 @@
-<html><head><title>InternalPage</title></head><body>
-<table><th>something…
-<td><script>/*</td><td>something…</td>
-<td>*/My JS attack payload/*</td><td>something…</td>
-<td>*/My JS attack payload/*</td><td>something…</td>
-<td>*/</script></td><td>something…</td>
-</table></body></html>
